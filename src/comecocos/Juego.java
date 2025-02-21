9373beac8c0badaee5fae2700a288d73120f8363
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-02-21 18:46:46 +0100

    Update and rename juego.java to Juego.java

---
+++ src/comecocos/Juego.java
@@ -0,0 +1,14 @@
+package comecocos;
+
+
+
+public class Juego {
+	//Main
+	public static void main(String[]args) {
+			
+		Mapa nuevoMapa = new Mapa();
+		nuevoMapa.generarMapa();
+	        nuevoMapa.jugar();
+	      
+	}
+}
